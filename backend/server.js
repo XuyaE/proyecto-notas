2e683ec859ae97eb30a5b584de9a0eced9a3082b
Xuya <edgarxuya37@gmail.com>
2026-03-10 16:00:33 -0600

    Cambios para preparar proyecto para produccion

--- backend/server.js
+++ backend/server.js
@@ -15,5 +15,5 @@ app.use('/usuarios', usuariosRoutes);
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log('Servidor corriendo en http://localhost:${PORT}');
+  console.log('Servidor corriendo en puerto ' + PORT);
 });
